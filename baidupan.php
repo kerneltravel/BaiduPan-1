b12ff764e09d9a8e9b2f25c5f41cc6f44b054c27
poboke <www@poboke.com>
2013-09-24 22:04:45 +0800

    Accommodate new link.

--- baidupan.php
+++ baidupan.php
@@ -1,15 +1,20 @@
 <?php
-//匹配shareid和uk参数
-preg_match('|\/(\d*)\/(\d*)\.|', $_SERVER["REQUEST_URI"], $res);
-list($shareid, $uk) = array_slice($res, 1, 2);
- 
-//构造百度网盘分享网址获取源码
-$url = "http://pan.baidu.com/share/link?shareid=$shareid&uk=$uk";
-$src = file_get_contents($url);
- 
+//构造百度网盘分享网址
+$uri = $_SERVER["REQUEST_URI"];
+if (strpos($uri, "?")) {
+    preg_match('|\/(\d+)\/(\d+)\.|', $uri, $res);
+    list($shareid, $uk) = array_slice($res, 1, 2);
+    $url = "http://pan.baidu.com/share/link?shareid=$shareid&uk=$uk";
+} else {
+    preg_match('|\/.+\/(\w+)\.|', $uri, $res);
+    $url = "http://pan.baidu.com/s/".$res[1];
+}
+
 //匹配源码里面的音乐地址并跳转
-preg_match('|MusicPlayer\("(.*)"|U', $src, $res);
+$src = file_get_contents($url);
+preg_match('|MusicPlayer\("(.+)"|U', $src, $res);
 $songurl = $res[1];
+
 //如果要外链其它格式的文件，可以反注释下面两行代码
 //preg_match('|dlink\\\":\\\"(.*)\\\"|U', $src, $res);
 //$songurl = str_replace("\\\\", "", $res[1]);
